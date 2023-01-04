1050b7391abf3bc949edbf7f5b0b7c038e0c1b4b
Benyamin Galeano <benyamin.galeano@gmail.com>
2023-01-04 00:27:15 -0600

    test: 💍 stats api

---
+++ tests/Stats/Factories/StatFactory.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests\Stats\Factories;
+
+use App\Stats\Domain\Models\Stat;
+use Faker\Factory;
+
+class StatFactory
+{
+  public static function createBatch(int $size): array
+  {
+    return array_map(fn() => self::create(), range(1, $size));
+  }
+
+  public static function create(
+    ?string $query = null,
+    ?int $searches = null,
+  ): Stat
+  {
+    $faker = Factory::create();
+    return new Stat(
+      query: $query ?? $faker->word(),
+      searches: $searches ?? $faker->numberBetween(0, 100)
+    );
+  }
+}
