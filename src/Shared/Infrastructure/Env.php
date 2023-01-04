caf14c48710c3be309cb91eae490b06c009d52a2
Benyamin Galeano <benyamin.galeano@gmail.com>
2023-01-03 20:39:09 -0600

    feat: 🎸 better way to get defaults from Env

--- src/Shared/Infrastructure/Env.php
+++ src/Shared/Infrastructure/Env.php
@@ -6,19 +6,28 @@ use Exception;
 
 class Env
 {
+  private static function getOrElse(string $key, mixed $default = null): mixed
+  {
+    if (!isset($_ENV[$key])) {
+      return $default;
+    }
+
+    return $_ENV[$key];
+  }
+
   public static function getCacheTTL(): int
   {
-    return (int) $_ENV['CACHE_TTL'] ?: 900;
+    return (int) self::getOrElse('CACHE_TTL', 900);
   }
 
   public static function getRedisHost(): string
   {
-    return $_ENV['REDIS_HOST'] ?: '127.0.0.1';
+    return self::getOrElse('REDIS_HOST', '127.0.0.1');
   }
 
   public static function getRedisPort(): int
   {
-    return (int) $_ENV['REDIS_PORT'] ?: 6379;
+    return (int) self::getOrElse('REDIS_PORT', 6379);
   }
 
   public static function getDefaultStartDateTime(): DateTime
@@ -28,26 +37,26 @@ class Env
 
   public static function getDbHost(): string
   {
-    return $_ENV['DB_HOST'] ?: '127.0.0.1';
+    return self::getOrElse('DB_HOST', '127.0.0.1');
   }
 
   public static function getDbPort(): int
   {
-    return (int) $_ENV['DB_PORT'] ?: 3306;
+    return (int) self::getOrElse('DB_PORT', 3306);
   }
 
   public static function getDbName(): string
   {
-    return $_ENV['MYSQL_DATABASE'] ?: throw new Exception('MYSQL_DATABASE not set');
+    return self::getOrElse('MYSQL_DATABASE') ?: throw new Exception('MYSQL_DATABASE not set');
   }
 
   public static function getDbUser(): string
   {
-    return $_ENV['MYSQL_USER'] ?: throw new Exception('MYSQL_USER not set');
+    return self::getOrElse('MYSQL_USER') ?: throw new Exception('MYSQL_USER not set');
   }
 
   public static function getDbPassword(): string
   {
-    return $_ENV['MYSQL_PASSWORD'] ?: throw new Exception('MYSQL_PASSWORD not set');
+    return self::getOrElse('MYSQL_PASSWORD') ?: throw new Exception('MYSQL_PASSWORD not set');
   }
 }
